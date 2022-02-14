4e4c5527520932308eff8cf8046052fcdda5d5ff
SouprP <potyrala.igor@gmail.com>
2022-02-14 15:35:29 +0100

    /bank top command, config fix

--- src/main/java/me/souprpk/main/Commands/BankerCommand.java
+++ src/main/java/me/souprpk/main/Commands/BankerCommand.java
@@ -1,6 +1,5 @@
 package me.souprpk.main.Commands;
 
-import com.google.common.collect.Maps;
 import me.souprpk.main.Banker;
 import me.souprpk.main.GUI.BankerGUI;
 import me.souprpk.main.Tools.Utilities;
@@ -11,8 +10,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class BankerCommand implements CommandExecutor {
 
@@ -49,9 +52,22 @@ public class BankerCommand implements CommandExecutor {
                         map = banker.flatData.getTopInfo();
                     }
                     try{
-                        map.forEach((uuid, money) -> {
-                            sender.sendMessage(Bukkit.getPlayer(UUID.fromString(uuid)).getName() + ": " + money);
+                        Map<String, BigDecimal> sorted = map.entrySet().stream().sorted(
+                                Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(10).collect(Collectors.toMap(
+                                        Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) ->
+                                 oldValue, LinkedHashMap::new));
+                        Utilities utils = new Utilities(banker);
+                        sender.sendMessage(utils.Translate(
+                                banker.getConfig().getString("main.prefix")
+                                        + banker.messageConfig.getConfig().getString("bankerTop")));
+                        AtomicInteger i = new AtomicInteger();
+                        sorted.forEach((k, v) -> {
+                            i.getAndIncrement();
+                            sender.sendMessage(utils.Translate("&a" + i + ". "
+                                    + "&8" + Bukkit.getPlayer(UUID.fromString(k)).getName()
+                                            + ": &a" + v + "$&8."));
                         });
+                        i.set(0);
                     }catch (Exception e){
 
                     }
